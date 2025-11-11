ea1826d751ec289f5adc9c85e67e0241d28d1ae6
Nayna Mittu <nayna@Naynas-MBP.communityfibre.co.uk>
2025-11-11 10:25:41 +0000

    update map in component to format date

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -7,25 +7,32 @@ const AllArticles = () => {
     fetch("https://nc-news-be-x7or.onrender.com/api/articles/")
       .then((res) => res.json())
       .then((data) => {
-        setArticles(data.articles);
-        console.log(data.articles);
+        const formattedDate = data.articles.map((article) => ({
+          ...article,
+          created_at: new Date(article.created_at).toLocaleString("en-GB", {timeStyle: "short", dateStyle: "short"}
+      )}));
+        setArticles(formattedDate);
+        // console.log(data.articles);
       });
   }, []);
 
-return (
-  <div>
-    <ul>
-      {articles.map((article) => (
-        <li className="articles" key={article.article_id}>
-          <h3>{article.title}</h3>
-          <p>By: {article.author}, </p>
-          <p>Topic: {article.topic}</p>
-          <img  src={article.article_img_url}/>
-          <p>Comments: {article.comment_count}</p>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
-}
+  return (
+    <div>
+      <ul>
+        {articles.map((article) => (
+          <li className="articles" key={article.article_id}>
+            <h3>{article.title}</h3>
+            <p>By: {article.author}, </p>
+            <p>Topic: {article.topic}</p>
+            <p>Posted: {article.created_at}</p>
+            <img src={article.article_img_url} />
+            <p>
+              Comments: {article.comment_count}, Votes: {article.votes}{" "}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default AllArticles;
